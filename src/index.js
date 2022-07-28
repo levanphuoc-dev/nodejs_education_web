b1c9746ac2ff1931dc8126559428fea63ceb1fdd
Lê Văn Phước <19522054@ms.uit.edu.vn>
2022-07-28 21:03:03 +0700

    Update code

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 
 const app = express();
-
 const port = 3000;
+
+const route = require('./routes');
 
 // public các file trong thư mục này.
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
 
 // HTTP logger
 app.use(morgan('combined'));
@@ -27,12 +33,7 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('news    ');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => console.log('App listening at http://localhost:%d', port));
